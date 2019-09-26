7020d13a0c555a66ac0952ed5b020bc67e25f09f
Al Atrash <alexthejumbo@gmail.com>
2019-09-26 11:16:07 +0200

    More tests for CreateRuleSet app

--- app/createRuleSet/test/createRuleSetSuccess_test.go
+++ app/createRuleSet/test/createRuleSetSuccess_test.go
@@ -4,6 +4,7 @@ import (
 	"bitbucket.verifone.com/validation-service/app/createRuleSet"
 	"bitbucket.verifone.com/validation-service/logger"
 	"bitbucket.verifone.com/validation-service/ruleSet"
+	"bitbucket.verifone.com/validation-service/ruleSet/rule"
 	"context"
 	"testing"
 )
@@ -28,9 +29,61 @@ func Test_App_CreateRuleSet_Success(t *testing.T) {
 	)
 
 	if err != nil {
-		t.Errorf("Failed to create rule set: %v", err)
+		t.Errorf("Failed to create RuleSet: %v", err)
 		return
 	}
 
 	assertRuleSet(t, mockRuleSet, *newRuleSet)
+}
+
+func Test_App_CreateRuleSet_Success_Amount_AllOperators(t *testing.T) {
+	amountOperators := []string{"<", "<=", "==", "!=", ">=", ">"}
+
+	for _, operator := range amountOperators {
+		log := logger.NewStubLogger()
+		repo, err := ruleSet.NewStubRepository()
+
+		if err != nil {
+			t.Errorf("Failed to init stub repository: %v", err)
+			return
+		}
+
+		app := createRuleSet.NewCreateRuleset(log, repo)
+
+		mockRules := []createRuleSet.Rule{
+			{
+				Key:      "amount",
+				Operator: operator,
+				Value:    "1000",
+			},
+		}
+
+		mockRuleSet := ruleSet.New(
+			mockRuleSet.EntityId,
+			mockRuleSet.Name,
+			mockRuleSet.Action,
+			[]rule.Metadata{
+				{
+					Property: "amount",
+					Operator: rule.Operator(operator),
+					Value:    "1000",
+				},
+			},
+		)
+
+		newRuleSet, err := app.Execute(
+			context.TODO(),
+			mockRuleSet.EntityId,
+			mockRuleSet.Name,
+			string(mockRuleSet.Action),
+			mockRules,
+		)
+
+		if err != nil {
+			t.Errorf("Failed to create RuleSet: %v", err)
+			return
+		}
+
+		assertRuleSet(t, mockRuleSet, *newRuleSet)
+	}
 }
--- app/createRuleSet/test/fixtures.go
+++ app/createRuleSet/test/fixtures.go
@@ -14,12 +14,47 @@ var mockRules = []createRuleSet.Rule{
 	},
 }
 
-var (
-	mockRuleSet = ruleSet.New("12345", "Test", ruleSet.Block, []rule.Metadata{
+var mockInvalidOperatorRules = []createRuleSet.Rule{
+	{
+		Key:      "amount",
+		Operator: "!!!",
+		Value:    "1000",
+	},
+}
+
+var mockInvalidKeyRules = []createRuleSet.Rule{
+	{
+		Key:      "invalid_field",
+		Operator: ">=",
+		Value:    "1000",
+	},
+}
+
+var mockInvalidValueRules = []createRuleSet.Rule{
+	{
+		Key:      "amount",
+		Operator: "<=",
+		Value:    "fff",
+	},
+}
+
+var mockInvalidNoValueRules = []createRuleSet.Rule{
+	{
+		Key:      "amount",
+		Operator: "<=",
+		Value:    "",
+	},
+}
+
+var mockRuleSet = ruleSet.New(
+	"12345",
+	"Test",
+	ruleSet.Block,
+	[]rule.Metadata{
 		{
 			Property: "amount",
 			Operator: ">=",
 			Value:    "1000",
 		},
-	})
+	},
 )
