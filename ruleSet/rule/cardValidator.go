a8295ab633a7e2811da10e84bcf20c108a495c3b
Al Atrash <alexthejumbo@gmail.com>
2020-12-10 14:53:23 +0100

    Skip card validation, skip issuerCountryCode validation if instrument is not provided

--- ruleSet/rule/cardValidator.go
+++ ruleSet/rule/cardValidator.go
@@ -1,29 +1,20 @@
 package rule
 
 import (
-	"bitbucket.verifone.com/validation-service/ruleSet/compare"
 	"bitbucket.verifone.com/validation-service/transaction"
 )
 
-type cardValidator struct {
-	comparator compare.StringComparator
+type dummyCardValidator struct {
 }
 
-func newCardValidator(operator Operator, value string) (*cardValidator, error) {
-	var comparator compare.StringComparator
-
-	switch operator {
-	case equal:
-		comparator = compare.EqualString(value)
-	case notEqual:
-		comparator = compare.NotEqualString(value)
-	default:
+func newCardValidator(operator Operator, value string) (*dummyCardValidator, error) {
+	if operator != equal && operator != notEqual {
 		return nil, InvalidOperatorError
 	}
 
-	return &cardValidator{comparator}, nil
+	return &dummyCardValidator{}, nil
 }
 
-func (c cardValidator) Validate(transaction transaction.Transaction) bool {
-	return c.comparator(transaction.Card)
+func (c dummyCardValidator) Validate(transaction transaction.Transaction) bool {
+	return false
 }
--- ruleSet/rule/issuerCountryCodeValidator.go
+++ ruleSet/rule/issuerCountryCodeValidator.go
@@ -29,5 +29,9 @@ func newIssuerCountryCodeValidator(operator Operator, value string) (*issuerCoun
 }
 
 func (i issuerCountryCodeValidator) Validate(transaction transaction.Transaction) bool {
+	if transaction.IssuerCountryCode == "" {
+		return false
+	}
+
 	return i.comparator(string(transaction.IssuerCountryCode))
 }
