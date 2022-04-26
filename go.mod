1cf0f74ea9cf304704e2277932f95d0df878f7a4
Al Atrash <alexthejumbo@gmail.com>
2022-04-26 14:51:12 +0200

    Reverting to original way of reading config to try and fix RPM

--- go.mod
+++ go.mod
@@ -17,14 +17,16 @@ require (
 	github.com/google/uuid v1.1.1
 	github.com/ilyakaznacheev/cleanenv v1.2.5
 	github.com/jarcoal/httpmock v1.1.0
+	github.com/jessevdk/go-flags v1.5.0
 	github.com/joho/godotenv v1.4.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/stretchr/testify v1.7.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.1
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- main.go
+++ main.go
@@ -5,12 +5,55 @@ import (
 	"os"
 	"validation-service/cmd"
 	"validation-service/config"
+
+	"github.com/jessevdk/go-flags"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	config.Read("")
+var version = "1.0.13"
+var appName = "Validation Service"
 
-	err := cmd.StartServer(config.App)
+type ConfigFileOpts struct {
+	ConfigFile string `long:"config" short:"f" default:"config.yml" description:"YAML configuration file path"`
+}
+
+func main() {
+	config.AppName = appName
+	config.Version = version
+
+	var opts ConfigFileOpts
+
+	_, err := flags.Parse(&opts)
+
+	if err != nil {
+		log.Printf("[ERROR] failed to parse arguments with error: %+v", err)
+		os.Exit(1)
+	}
+
+	file, err := os.Open(opts.ConfigFile)
+
+	if err != nil {
+		log.Printf("[ERROR] failed to read configuration with error: %+v", err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+
+	var serverConfig config.Server
+
+	if err := decoder.Decode(&serverConfig); err != nil {
+		log.Printf("[ERROR] failed to decode configuration with error: %+v", err)
+		os.Exit(1)
+	}
+
+	if err := serverConfig.Validate(); err != nil {
+		log.Printf("[ERROR] configuration is invalid: %+v", err)
+		os.Exit(1)
+	}
+
+	err = cmd.StartServer(serverConfig)
 
 	if err != nil {
 		log.Printf("[ERROR] server command failed with error: %+v", err)
